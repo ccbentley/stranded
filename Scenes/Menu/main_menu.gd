0ea675e55229a20201f11c92cd718d89d167c19c
Carter <136651657+ccbentley@users.noreply.github.com>
2024-07-02 23:02:50 -0400

    Rebind controls options menu

--- Scenes/Menu/main_menu.gd
+++ Scenes/Menu/main_menu.gd
@@ -2,22 +2,24 @@ extends Control
 class_name MainMenu
 
 @onready var play_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/PlayButton
-@onready var settings_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/SettingsButton
+@onready var options_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/OptionsButton
 @onready var quit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
 
 @onready var margin_container: MarginContainer = $MarginContainer
 @onready var save_selection_menu: SaveSelectionMenu = $SaveSelectionMenu
 @onready var create_world_menu: CreateWorldMenu = $CreateWorldMenu
+@onready var options_menu: OptionsMenu = $OptionsMenu
 
 func _ready() -> void:
 	handle_connecting_signals()
 
 func handle_connecting_signals() -> void:
 	play_button.button_down.connect(on_play_pressed)
-	settings_button.button_down.connect(on_settings_pressed)
+	options_button.button_down.connect(on_options_pressed)
 	quit_button.button_down.connect(on_quit_pressed)
 	save_selection_menu.exit_save_selection_menu.connect(on_exit_save_selection_menu)
 	create_world_menu.exit_create_world_menu.connect(on_exit_create_world_menu)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
 
 func on_play_pressed() -> void:
 	margin_container.visible = false
@@ -28,8 +30,15 @@ func on_play_pressed() -> void:
 func on_quit_pressed() -> void:
 	get_tree().quit()
 
-func on_settings_pressed() -> void:
-	pass
+func on_options_pressed() -> void:
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+
+func on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options_menu.set_process(false)
+	options_menu.visible = false
 
 func on_exit_save_selection_menu() -> void:
 	margin_container.visible = true
