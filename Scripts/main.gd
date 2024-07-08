b994a188296cde8f676653597b58af22835e4076
Carter <136651657+ccbentley@users.noreply.github.com>
2024-07-07 20:41:09 -0400

    Animated tiles

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -14,8 +14,10 @@ var playerData: PlayerData = PlayerData.new()
 
 @export var noise_height_text : NoiseTexture2D
 @export var noise_tree_text : NoiseTexture2D
+@export var noise_water_text : NoiseTexture2D
 var noise : Noise
 var tree_noise : Noise
+var water_noise : Noise
 @onready var tile_map: TileMap = $TileMap
 var source_id : int = 0
 
@@ -30,6 +32,7 @@ var water_atlas : Vector2i = Vector2i(8, 4)
 var land_atlas : Vector2i = Vector2i(1, 1)
 var sand_tiles_arr : Array = []
 var grass_tiles_arr : Array = []
+var water_bubble_atlas_arr : Array = [Vector2i(28,2), Vector2i(29,2), Vector2i(30,2), Vector2i(31,2)]
 var terrain_sand_int : int = 0
 var terrain_grass_int : int = 1
 
@@ -52,9 +55,11 @@ func _ready() -> void:
 
 	noise = noise_height_text.noise
 	tree_noise = noise_tree_text.noise
+	water_noise = noise_water_text.noise
 
 	noise.seed = Global.worldData.world_seed
 	tree_noise.seed = randi()
+	water_noise.seed = randi()
 
 	save_game()
 
@@ -77,7 +82,8 @@ func generate_chunk(x_chunk, y_chunk) -> void:
 			var y = _y + y_chunk * 32
 			var noise_val : float = noise.get_noise_2d(x, y)
 			var tree_noise_val : float = tree_noise.get_noise_2d(x, y)
-			if noise_val > 0.3:
+			var water_noise_val : float = water_noise.get_noise_2d(x, y)
+			if noise_val >= 0.3:
 				sand_tiles_arr.append(Vector2i(x, y))
 				# Add adjacent tiles to expanded_sand_tiles_arr
 				for dx in range(-2, 3):
@@ -85,7 +91,7 @@ func generate_chunk(x_chunk, y_chunk) -> void:
 						var adj_tile = Vector2i(x + dx, y + dy)
 						if not expanded_sand_tiles_arr.has(adj_tile) and is_in_chunk(Vector2i(x_chunk, y_chunk), adj_tile):
 							expanded_sand_tiles_arr.append(adj_tile)
-			if noise_val > 0.4:
+			if noise_val >= 0.4:
 				grass_tiles_arr.append(Vector2i(x, y))
 				# Add adjacent tiles to expanded_grass_tiles_arr
 				for dx in range(-1, 2):
@@ -93,13 +99,15 @@ func generate_chunk(x_chunk, y_chunk) -> void:
 						var adj_tile = Vector2i(x + dx, y + dy)
 						if not expanded_grass_tiles_arr.has(adj_tile) and is_in_chunk(Vector2i(x_chunk, y_chunk), adj_tile):
 							expanded_grass_tiles_arr.append(adj_tile)
-				if tree_noise_val > 0.6:
+				if tree_noise_val > 0.7:
 					var tree_obj = TREE.instantiate()
 					$Trees.add_child(tree_obj)
 					tree_obj.position = tile_map.map_to_local(Vector2i(x, y))
-			else:
+			if noise_val <= 0.4:
 				if not expanded_sand_tiles_arr.has(Vector2i(x, y)) and not expanded_grass_tiles_arr.has(Vector2i(x, y)):
-					tile_map.set_cell(water_layer, Vector2(x, y), source_id, water_atlas)
+					tile_map.set_cell(water_layer, Vector2i(x, y), source_id, water_atlas)
+					if water_noise_val > 0.8:
+						tile_map.set_cell(environment_layer, Vector2i(x, y), source_id, water_bubble_atlas_arr.pick_random())
 	tile_map.set_cells_terrain_connect(ground_1_layer, expanded_sand_tiles_arr, terrain_sand_int, 0)
 	tile_map.set_cells_terrain_connect(ground_2_layer, expanded_grass_tiles_arr, terrain_grass_int, 0)
 
