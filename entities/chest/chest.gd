a7aef8f11f24577a4b1d416ba1fc7c00592045bd
Carter <136651657+ccbentley@users.noreply.github.com>
2024-08-03 23:34:38 -0400

    Fixed external inventory crash

--- entities/chest/chest.gd
+++ entities/chest/chest.gd
@@ -5,5 +5,5 @@ signal toggle_inventory(external_inventory_owner: Node2D)
 @export var inventory_data: InventoryData
 
 
-func player_interact() -> void:
+func player_interact(_player: Player) -> void:
 	toggle_inventory.emit(self)
