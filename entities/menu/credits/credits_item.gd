eeab16a1e21543a780032a26d855b84f1c2d0512
Carter <136651657+ccbentley@users.noreply.github.com>
2024-08-17 16:45:40 -0400

    Reformat credits menu

--- entities/menu/credits/credits_item.gd
+++ entities/menu/credits/credits_item.gd
@@ -2,11 +2,20 @@ extends Control
 
 @export var title: String = "Title"
 @export var author: String = "Author"
+@export var link: String = "Link"
 
-@onready var title_label: Label = $HBoxContainer/Title
-@onready var author_label: Label = $HBoxContainer/Author
+@onready var title_label: Label = $MarginContainer/Title/Title
+@onready var author_label: Label = $MarginContainer/Title/Author/Author
+@onready var link_button: LinkButton = $MarginContainer/Title/Author/LinkButton
+@onready var dash: Label = $MarginContainer/Title/Author/Dash
 
 
 func _ready() -> void:
 	title_label.text = title
 	author_label.text = author
+	link_button.text = link
+	link_button.uri = link
+	if link == "":
+		dash.visible = false
+	else:
+		dash.visible = true
