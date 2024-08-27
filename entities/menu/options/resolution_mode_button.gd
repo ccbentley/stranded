58fa3a9b981d2a0a4ad6b779a9e94afc8303c322
Carter <136651657+ccbentley@users.noreply.github.com>
2024-08-27 16:39:36 -0400

    More resolution options

--- entities/menu/options/resolution_mode_button.gd
+++ entities/menu/options/resolution_mode_button.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton
 
-const RESOLUTION_DICTONARY: Dictionary = {"1152 x 648": Vector2i(1152, 648), "1280 x 720": Vector2i(1280, 720), "1920 x 1080": Vector2i(1920, 1080)}
+const RESOLUTION_DICTONARY: Dictionary = {"1152 x 648": Vector2i(1152, 648), "1280 x 720": Vector2i(1280, 720), "1600 x 900": Vector2i(1600, 900), "1920 x 1080": Vector2i(1920, 1080)}
 
 
 func _ready() -> void:
