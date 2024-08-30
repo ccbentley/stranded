8a1e07eda53863bb5657d3a940798005d4afddd4
Carter <136651657+ccbentley@users.noreply.github.com>
2024-08-30 09:40:59 -0400

    Fix day counter bug

--- utilities/day_night_cycle/day_night_cycle.gd
+++ utilities/day_night_cycle/day_night_cycle.gd
@@ -24,5 +24,5 @@ func next_day() -> void:
 
 func set_current_time(new_time: float) -> void:
 	animation_player.advance(new_time / animation_player.speed_scale)
-	if new_time >= total_time / 2:
+	if animation_player.current_animation_position >= total_time / 2:
 		day_counter -= 1
